795351755d00ffb59b489132a4433d412b13ce36
Vaibhav Raizada <Vaibhav Raizada@users.noreply.github.com>
2016-01-05 13:35:55 +0530

    First Commit

---
+++ src/main/java/com/sbux/rest/SbuxMainClass.java
@@ -0,0 +1,24 @@
+package com.sbux.rest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class SbuxMainClass {
+
+	public static void main(String[] args) {
+	
+		Map<String,String> urlVariables = new HashMap<String, String>();
+		urlVariables.put("id", "1");
+		
+		RestTemplate restClient = new RestTemplate();
+		
+		ResponseEntity<String> response =  restClient.getForEntity("http://jsonplaceholder.typicode.com/posts/{id}", String.class, urlVariables);
+		
+		System.out.println("Response Body>>>"+response.getBody());
+		
+	}
+
+}
